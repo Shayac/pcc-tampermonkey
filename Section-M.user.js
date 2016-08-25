5a9a29a9492b655ec6833177b711cfcbb956464c
Shaya Cohen <shayac1@gmail.com>
2016-08-24 22:11:24 -0400

    click none of the above, remove jquery

--- Section-M.user.js
+++ Section-M.user.js
@@ -2,16 +2,16 @@
 // @name         Section M
 // @namespace    https://www4.pointclickcare.com/clinical/mds3/section.xhtml
 // @version      0.1
-// @description  Default input 0
+// @description  Click none of the above and then select first 2
 // @author       Shaya Cohen
 // @include      https://www4.pointclickcare.com/clinical/mds3/section.xhtml?*&sectioncode=M
 // @grant        none
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     $(function(){
+        $('#M1200Z_noneoftheabove').click();
         $('#M1200A_wrapper').find('a[data-value="1"]').click();
         $('#M1200B_wrapper').find('a[data-value="1"]').click();
     });
