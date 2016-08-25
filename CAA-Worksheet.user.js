29e41573258c739ba9b561e0cb6dc75a70e4494d
Shaya Cohen <shayac1@gmail.com>
2016-08-24 21:02:12 -0400

    remove jquery and add keyup

--- CAA-Worksheet.user.js
+++ CAA-Worksheet.user.js
@@ -5,12 +5,12 @@
 // @author       Shaya Cohen
 // @include      https://www4.pointclickcare.com/clinical/mds/caaworksheet.xhtml*
 // @grant        none
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js
+
 // ==/UserScript==
 
 (function() {
     'use strict';
     $(function(){
-        $('#txt_prob').val('See note below');
+        $('#txt_prob').val('See note below').keyup();
     });
 })();
