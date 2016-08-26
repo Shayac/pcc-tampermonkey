cc881db36a5075377ad7a7bb44e4b2ab2ba76be0
Shaya Cohen <shayac1@gmail.com>
2016-08-26 07:32:17 -0400

    query if value

--- Section-P.user.js
+++ Section-P.user.js
@@ -16,3 +16,19 @@
         }
     });
 })();
+
+/*
+
+ const questions = $('.question_content').filter(function(){
+             const div = this;
+             const value = $(this).find('input:not([id^="ack"])').val();
+            const hasValue = !isNaN(parseInt(value));
+            if(!hasValue) {
+                return true;
+            }
+            return false;
+        });
+    const links = questions.find('ul.responses>li>a[data-value="0"]');
+    console.log(links);
+    
+    */
