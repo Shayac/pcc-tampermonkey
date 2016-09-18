503e5d6a49344a13ee46b4b4fcb7a38da98c5d55
Shaya Cohen <shayac1@gmail.com>
2016-09-18 10:14:10 -0400

    section N into shift-click

--- Section-N.user.js
+++ Section-N.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         Section N
-// @namespace    https://www4.pointclickcare.com/clinical/mds3/section.xhtml
-// @version      0.1
-// @description  Default input 0
+// @version      0.2
+// @description  Shift-Click default input 0
 // @author       Shaya Cohen
 // @include      https://www4.pointclickcare.com/clinical/mds3/section.xhtml?*&sectioncode=N
 // @grant        none
@@ -11,10 +10,14 @@
 (function() {
     'use strict';
     $(function(){
-        $('div.questiongroup').find('input:not([type="hidden"])').each(function(index){
-            // if input doesn't contains a value change val to 0
-            if (!/[0-7]/.test($(this).val())){
-                $(this).val(0);
+        $('body').click(function (e) {
+            if (e.shiftKey) {
+                $('div.questiongroup').find('input:not([type="hidden"])').each(function(index){
+                    // if input doesn't contains a value change val to 0
+                    if (!/[0-7]/.test($(this).val())){
+                        $(this).val(0);
+                    }
+                });
             }
         });
     });
